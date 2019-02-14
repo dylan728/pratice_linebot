1c32dc74002cdf00c338733dd59629391d32f4b2
dylan728 <47588248+dylan728@users.noreply.github.com>
2019-02-14 15:23:47 +0800

    Update vision.php

--- vision.php
+++ vision.php
@@ -2,7 +2,7 @@
     $json_str = file_get_contents('php://input'); //接收request的body
     $json_obj = json_decode($json_str); //轉成json格式
   
-    $myfile = fopen("log.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
+    $myfile = fopen("log111.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
     fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
     
     $sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
